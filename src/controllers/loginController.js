ea7976547546e41f222a3f504e028c8bde881ea3
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-04-09 19:07:06 -0300

    Alterado loginControler para direcionar para página inicial

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,9 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-    res.render('login')
-};
-
+    if(req.session.user) return res.render('login-logado')
+    return res.render('login');
+}
 exports.register = async function(req, res) {
     try{
         const login = new Login(req.body);
@@ -11,19 +11,44 @@ exports.register = async function(req, res) {
     
         if(login.errors.length > 0){
             req.flash('errors', login.errors);
-            req.session.save(function(){
-                return res.location(req.get("Referrer"))
-            });
-            return;
+          req.session.save(() => {
+            return res.redirect("/login/index")
+        });
+        return;
         }
     
         req.flash('success', 'Seu cadastro foi realizado com sucesso.');
-        req.session.save(function(){
-            return res.location(req.get("Referrer"))
-        });
+        return req.session.save(() => res.redirect("/login/index"));
     } catch(e) {
         console.log(e);
         return res.render('404');
     }
    
 };
+
+exports.login = async function(req, res) {
+    try{
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(() =>{ 
+                return res.redirect("/login/index")
+            });
+            return;
+        }
+       
+        req.flash('success', 'Login realizado com sucesso.');
+        req.session.user = login.user;
+        return req.session.save(() => res.redirect("/login/index"));
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+   
+};
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect("/login/index");
+}
