90d5faa68c30fe0272f6b77b59fdb3bb89780edd
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-04-13 18:02:43 -0300

    Realizado importação da classe Login, criado instâncias para os formulários de login e cadastro com seletores CSS, e inicializado ambos os formulários chamando o método init().

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,11 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import "./assets/css/style.css";
+// import "./assets/css/style.css";
 
+import Login from './modules/logins';
+
+const login = new Login('.form-login');
+const cadastro  = new Login('.form-cadastro');
+cadastro.init();
+login.init();
 
